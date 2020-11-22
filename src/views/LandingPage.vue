56c67d677630244d972125db69ef735247223063
Pierre Fabarius <pierre.fabarius@gmail.com>
2020-11-22 16:54:53 +0100

    design changes: landingpage and nav

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,72 +1,32 @@
 <template>
-  <div class="p-5">
+  <div class="container-fluid">
     <div id="demo" class="carousel slide" data-ride="carousel">
       <!-- Indicators -->
       <ul class="carousel-indicators">
         <li data-target="#demo" data-slide-to="0" class="active"></li>
         <li data-target="#demo" data-slide-to="1"></li>
-        <li data-target="#demo" data-slide-to="2"></li>
       </ul>
 
       <!-- The slideshow -->
-      <div class="carousel-inner rounded-pill">
+      <div class="carousel-inner">
         <div class="carousel-item active">
-          <a href="/berlin">          
-            <img src="/images/berlin.jpg" class="img-fluid" alt="Los Angeles" />
-          </a>
+            <img src="/images/berlin-alex.jpg" class="img-fluid" alt="Alexanderplatz" />
         </div>
         <div class="carousel-item">
-          <a href="/hamburg">              
-            <img src="/images/hamburg.jpg" class="img-fluid" alt="Chicago" />
-          </a>
-        </div>
-        <div class="carousel-item">
-          <a href="/munich">              
-            <img src="/images/munich.jpg" class="img-fluid" alt="New York" />
-          </a>
+            <img src="/images/berlin-brandenburger_tor.jpg" class="img-fluid" alt="Brandenburger Tor" />
         </div>
       </div>
+    </div>
 
-      <!-- Left and right controls -->
-      <a class="carousel-control-prev" href="#demo" data-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </a>
-      <a class="carousel-control-next" href="#demo" data-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </a>
+    <div class="jumbotron">
+      <h1 class="display-4">City Data Visualizer</h1>
+      <p class="lead">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+      <hr class="my-4">
+      <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. </p>
+      <a class="btn btn-primary btn-lg" href="/berlin/karte/corona" role="button">Start</a>
     </div>
-    <hr class="my-3" />
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <a href="/berlin">               
-          <img
-            src="/images/berlin.jpg"
-            class="w-100 rounded-pill"
-            alt="Responsive image"
-          />
-          </a>
-        </div>
-        <div class="col">
-          <a href="/hamburg">               
-          <img
-            src="/images/hamburg.jpg"
-            class="w-100 rounded-pill"
-            alt="Responsive image"
-          />
-          </a>
-        </div>
-        <div class="col">
-          <a href="/munich">               
-          <img
-            src="/images/munich.jpg"
-            class="w-100 rounded-pill"
-            alt="Responsive image"
-          />
-          </a>               
-        </div>
-      </div>
-    </div>
+
+
   </div>
 </template>
 
@@ -87,4 +47,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .container-fluid{
+    padding: 0;
+  }
 </style>
