3f830a8987015d8aaa6246a9f69e5bf4fb8f6280
wkrl <kwxrl@yandex.com>
2020-12-01 16:19:42 +0100

    Cleanup

--- src/assets/GeneralClasses.js
+++ src/assets/GeneralClasses.js
@@ -1,36 +1,32 @@
 import Dictionary from "../assets/Dictionary.json";
 
-var FinalWord = "";
+let FinalWord = "";
 
 export default {
-    // GETAPI:function() { return "https://pthr.uber.space/padp"; },
-    GETAPIberlincoviddistrict:function() { return "https://cdv-backend.api.datexis.com/api/v1/berlin-covid-district"; },    
-    GETAPIberlinshapesdistrict:function() { return "https://cdv-backend.api.datexis.com/api/v1/berlin-shapes-district"; },    
-    GETAPIberlincovidage:function() { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-age"; },
-    GETAPIberlinaccidents:function() { return "https://cdv-backend.api.datexis.com//api/v1/berlin-accidents";},
-    GETAPIberlinHospitals:function() { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-intensive-care/latest";},
+	// GETAPI:function() { return "https://pthr.uber.space/padp"; },
+	GETAPIberlincoviddistrict: function () { return "https://cdv-backend.api.datexis.com/api/v1/berlin-covid-district"; },
+	GETAPIberlinshapesdistrict: function () { return "https://cdv-backend.api.datexis.com/api/v1/berlin-shapes-district"; },
+	GETAPIberlincovidage: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-age"; },
+	GETAPIberlinaccidents: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-accidents"; },
+	GETAPIberlinHospitals: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-intensive-care/latest"; },
 
+	GETberlindistrics: function () { return ["Berlin Reinickendorf", "Berlin Charlottenburg-Wilmersdorf", "Berlin Treptow-Köpenick", "Berlin Pankow", "Berlin Neukölln", "Berlin Lichtenberg", "Berlin Marzahn-Hellersdorf", "Berlin Spandau", "Berlin Steglitz-Zehlendorf", "Berlin Mitte", "Berlin Friedrichshain-Kreuzberg", "Berlin Tempelhof-Schöneberg"] },
 
-    ValueLanguage:function() { return "tduAjTngKL"; },     
-    LSLanguage:function() { return localStorage.getItem(this.ValueLanguage()); }, 
+	ValueLanguage: function () { return "tduAjTngKL"; },
+	LSLanguage: function () { return localStorage.getItem(this.ValueLanguage()); },
 
-    GetWord: function (String) 
-    {
-        try 
-        {
-            if (this.LSLanguage() == "DE") 
-            {
-                FinalWord = Dictionary.Words.find(Word => Word.Value === String).DE;
-            } 
-            else 
-            {
-                FinalWord = Dictionary.Words.find(Word => Word.Value === String).EN;
-            }
-            return FinalWord;
-        } 
-        catch 
-        {
-            console.log("Can't Find The Word : " + String);
-        }
-    },    
+	GetWord: function (String) {
+		try {
+			if (this.LSLanguage() == "DE") {
+				FinalWord = Dictionary.Words.find(Word => Word.Value === String).DE;
+			}
+			else {
+				FinalWord = Dictionary.Words.find(Word => Word.Value === String).EN;
+			}
+			return FinalWord;
+		}
+		catch {
+			console.log("Can't Find The Word: " + String);
+		}
+	},
 }
