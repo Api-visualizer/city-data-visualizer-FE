cf2e50ba940abd91f382a091485f1cd538ccd56b
wkrl <kwxrl@yandex.com>
2020-12-02 16:20:36 +0100

    Move Main.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <CovidAgeBarchart />
+    <CovidCasesLinechart />
+  </div>
+</template>
+
+<script>
+import CovidAgeBarchart from '@/components/charts/CovidAgeBarchart';
+import CovidCasesLinechart from '@/components/charts/CovidCasesLinechart';
+
+export default {
+  name: "Main",
+
+  components: {
+    CovidAgeBarchart,
+    CovidCasesLinechart
+  },
+
+  props: {},
+
+  data () {
+    return {}
+  },
+
+  methods: {},
+
+  mounted() {},
+};
+</script>
+
+<style scoped>
+</style>
