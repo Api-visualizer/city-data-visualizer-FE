c4d3953a404c17fffb89d2bba1e07f764dbe4a65
kat-hi <s77518@beuth-hochschule.de>
2020-12-19 17:23:29 +0100

    use preprocessed data

--- src/assets/GeneralClasses.js
+++ src/assets/GeneralClasses.js
@@ -6,7 +6,7 @@ export default {
 	GETAPIberlincoviddistrict: function () { return "https://cdv-backend.api.datexis.com/api/v1/berlin-covid-district"; },
 	GETAPIberlinshapesdistrict: function () { return "https://cdv-backend.api.datexis.com/api/v1/berlin-shapes-district"; },
 	GETAPIberlincovidage: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-age"; },
-	GETAPIberlinaccidents: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-accidents"; },
+	GETAPIberlinaccidents: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-accidents-new"; },
 	GETAPIberlinHospitals: function () { return "https://cdv-backend.api.datexis.com//api/v1/berlin-covid-intensive-care"; },
 	GetAPIBerlinCancerCases: function () { return " https://cdv-backend.api.datexis.com/api/v1/berlin-cancer" },
 	GETberlindistrics: function () { return ["Berlin Reinickendorf", "Berlin Charlottenburg-Wilmersdorf", "Berlin Treptow-Köpenick", "Berlin Pankow", "Berlin Neukölln", "Berlin Lichtenberg", "Berlin Marzahn-Hellersdorf", "Berlin Spandau", "Berlin Steglitz-Zehlendorf", "Berlin Mitte", "Berlin Friedrichshain-Kreuzberg", "Berlin Tempelhof-Schöneberg"] },
