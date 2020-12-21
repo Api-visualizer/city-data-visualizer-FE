e8e72f84382af23a176085cc9e61604915aa12b2
pierrefabarius <pierre.fabarius@gmail.com>
2020-12-21 12:42:59 +0100

    [ADD] legal info

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -44,6 +44,32 @@
       <a class="btn btn-primary" href="/#contact" role="button">Contact</a>
     </div>
 
+    <div class="jumbotron ">
+      <div class="card" style="width: 60%;">
+        <h2 class="display-4">Legal Disclosure</h2>
+        <div class="card-body ">
+          <h5 class="card-title">Information in accordance with Section 5 TMG</h5>
+            <br>
+          <h6 class="card-subtitle mb-2 text-muted">Accountability for content</h6>
+          <p class="card-text text-left">The contents of our pages have been created with the utmost care. However, we cannot guarantee the contents' accuracy, completeness or topicality. 
+          According to statutory provisions, we are furthermore responsible for our own content on these web pages. 
+          In this matter, please note that we are not obliged to monitor the transmitted or saved information of third parties, or investigate circumstances pointing to illegal activity. 
+          Our obligations to remove or block the use of information under generally applicable laws remain unaffected by this as per §§ 8 to 10 of the Telemedia Act (TMG).</p>
+            <br>
+          <h6 class="card-subtitle mb-2 text-muted">Accountability for links</h6>
+          <p class="card-text text-left">Responsibility for the content of external links (to web pages of third parties) lies solely with the operators of the linked pages. 
+            No violations were evident to us at the time of linking. Should any legal infringement become known to us, we will remove the respective link immediately.</p>
+            <br>
+          <h6 class="card-subtitle mb-2 text-muted">Copyright</h6>
+          <p class="card-text text-left">Our web pages and their contents are subject to German copyright law. Unless expressly permitted by law, every form of utilizing, 
+            reproducing or processing works subject to copyright protection on our web pages requires the prior consent of the respective owner of the rights. 
+            Individual reproductions of a work are only allowed for private use. The materials from these pages are copyrighted and any unauthorized use may violate copyright laws.</p>
+          <!-- <a href="#" class="card-link">Card link</a>
+          <a href="#" class="card-link">Another link</a> -->
+        </div>
+      </div>        
+    </div>
+
   </div>
 </template>
 
@@ -136,4 +162,11 @@ p {
 .btn-primary {
   color: white;
 }
+
+.card {
+  margin: 0 auto; 
+  float: none; 
+  margin-bottom: 10px; 
+}
+
 </style>
