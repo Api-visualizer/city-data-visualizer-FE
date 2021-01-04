6192f1071eee7c78e7f7ea6ce87885f4bf0e229f
Niklas Rupprecht <s71551@beuth-hochschule.de>
2021-01-04 16:32:12 +0100

    legend gradient

--- src/components/maps/AccidentsMap.vue
+++ src/components/maps/AccidentsMap.vue
@@ -202,16 +202,29 @@ export default {
     customLegendControl: function () {
       let legend = L.control({ position: 'topleft' });
       legend.onAdd = function () {
-        let colors = ['#956bff', '#94f3ff', '#6ff765', '#fffc33', '#dea004', '#bd1806'];
 
-        let div = L.DomUtil.create('div', 'info legend'),
-          grades = ['min', '', '', '', '', 'max'];
+        let div = L.DomUtil.create('div', 'info legend')
         let label = '<div class="mb-4"><strong>Number<br/>of accidents</strong></div>';
         div.innerHTML += label;
 
-        for (let i = 0; i < grades.length; i++) {
-          div.innerHTML += "<div class='text-left mt-1 mb-1'><div class='text-left text-dark p-1 mr-2' style='display:inline; opacity:0.7; background:" + colors[i] + "'>&nbsp;&nbsp;</div><div style='display:inline;'>" + grades[i].toUpperCase() + '</div></div>';
-        }
+        div.innerHTML += `
+        <div class='container'>
+          <div class='row'>
+            <div class='gradient' style='display:inline; width:25px; background:linear-gradient(180deg, rgba(4,99,244,1) 11%, rgba(84,209,255,1) 41%, rgba(38,234,122,1) 52%, rgba(251,255,37,1) 65%, rgba(255,197,54,1) 74%, rgba(255,11,11,1) 100%);'></div>
+            <div class='col pt-0'>
+              <div class='row'>
+                <div style='display:inline;'>MIN</div>
+              </div>
+              <div class='row'>
+                <div style='display:block; height:100px;'></div>
+              </div>
+              <div class='row'>
+                <div style='display:inline;'>MAX</div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+        
         return div;
       };
       return legend;
