33a7a0c6180b202ecf22ad4f11e16fcf6156ea2c
Niklas Rupprecht <s71551@beuth-hochschule.de>
2021-01-11 13:53:24 +0100

    clickable arrow

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -33,15 +33,17 @@
         <h1 class="display-4">City Data Visualizer</h1>
         <p class="lead pt-4" style="padding-bottom: 24px;">City Data Visualizer provides helpful visualizations for complex health data. With the help of open data, we hope to give both Berlin's administration, scientists and residents a better understanding of current health risks or positive impacts that are occurring in the city.</p>
         <p class="text-center"></p>
-
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-double-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-          <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-        </svg>
+        
+        <a href="#topics" class="text-dark">
+          <svg href="#topics" width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chevron-double-down border rounded-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+          </svg>
+        </a>
 
     </div>
 
-      <div class="jumbotron">
+      <div class="jumbotron" id="topics">
         <div class="row">
 
           <div class="col-sm-6 d-flex align-items-stretch">
@@ -218,6 +220,10 @@ svg {
   animation-duration: 2s;
   animation-delay: 3s;
   animation-iteration-count: infinite;
+}
+
+svg:hover{
+  box-shadow: 0px 2px 2px 1px rgba(0,0,0,0.75);
 }
 
 @keyframes move {
