39705065cadb0e301128c859a1fbf996949eba20
Anas Alhakim <anasalhakim@gmail.com>
2021-01-26 17:32:18 +0100

    Fixes

--- src/components/charts/CancerCasesBarchart.vue
+++ src/components/charts/CancerCasesBarchart.vue
@@ -2,186 +2,179 @@
   <div>
     <h2>Cancer cases</h2>
     <h3>Yearly overview of cancer cases per age range</h3>
-     <div class="card">
-    <div class="row">
-    <div>
-      <div class="app">
-        <v-select :items="years" v-model="selectedYear" :dense="true" :menu-props="{ maxHeight: '150px' }" label="Select year" v-on:change="getDataOnChange(selectedYear)"> </v-select>
-      </div>
+    <div class="card">
       <div class="row">
-        <div class="col-8">
-          <div id="chart">
-            <apexchart type="bar" height="450" :options="options" :series="series"></apexchart>
+        <div>
+          <div class="app">
+            <v-select :items="years" v-model="selectedYear" :dense="true" :menu-props="{ maxHeight: '150px' }" label="Select year" v-on:change="getDataOnChange(selectedYear)"> </v-select>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div id="chart">
+                <apexchart type="bar" height="450" :options="options" :series="series"></apexchart>
+              </div>
+            </div>
+            <div class="col-3">
+              <Textbox :content="content" title="Cancer" subtitle="Case numbers per year" class="box" link="http://www.gsi-berlin.info/gsi_suchen.asp?seite=2&CBFest=Suchtext&Suchtext=tg0800373100202008%2Exls&Modus=9" />
+            </div>
           </div>
         </div>
-        <div class="col-3">
-          <Textbox :content="content" title="Cancer" subtitle="Case numbers per year" class="box" link="http://www.gsi-berlin.info/gsi_suchen.asp?seite=2&CBFest=Suchtext&Suchtext=tg0800373100202008%2Exls&Modus=9"/>
-        </div>
       </div>
-
-  </div>
+    </div>
   </div>
 </template>
 <script>
-import GeneralClasses from "@/assets/GeneralClasses";
-import "leaflet/dist/leaflet.css";
-import Textbox from "@/components/Textbox";
+import GeneralClasses from '@/assets/GeneralClasses';
+import 'leaflet/dist/leaflet.css';
+import Textbox from '@/components/Textbox';
 
 export default {
-    name: "CancerCasesBarchart",
+  name: 'CancerCasesBarchart',
   components: { Textbox },
-  props: {
-
-    },
-    data() {
-        return {
-          content: "This chart shows you how the number of cancer patients has developed through the years 2008 until 2017. The chart has many functionalities which enable you to investigate the exact information which you may need about cancer in Berlin, for example you can see the number of patients according to their age and gender as well as the total number of patients for each age group of people. You can filter the results (according to the selected year) to see the difference and the development of the number of cancer patients.",
-            data: {},
-            years: [],
-            selectedYear: '2008',
-            ages: [],
-            options: {
-              chart: {
-                type: 'bar',
-              },
-              plotOptions: {
-                bar: {
-                  horizontal: false,
-                  columnWidth: '55%',
-                  endingShape: 'rounded'
-                },
-              },
-              dataLabels: {
-                enabled: false
-              },
-              stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-              },
-              xaxis: {
-                categories: [""],
-                title: {
-                  text: 'Age of patients'
-                }
-              },
-              yaxis: {
-                title: {
-                  text: 'Number of cancer patients'
-                }
-              },
-              fill: {
-                opacity: 1
-              },
-
-              tooltip: {
-                y: {
-                  formatter: function (val) {
-                    return  val + " patients"
-                  }
-                }
-              }
+  props: {},
+  data() {
+    return {
+      content: 'This chart shows you how the number of cancer patients has developed through the years 2008 until 2017. The chart has many functionalities which enable you to investigate the exact information which you may need about cancer in Berlin, for example you can see the number of patients according to their age and gender as well as the total number of patients for each age group of people. You can filter the results (according to the selected year) to see the difference and the development of the number of cancer patients.',
+      data: {},
+      years: [],
+      selectedYear: '2008',
+      ages: [],
+      options: {
+        chart: {
+          type: 'bar',
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '55%',
+            endingShape: 'rounded',
           },
-          series: [],
-    }
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent'],
+        },
+        xaxis: {
+          categories: [''],
+          title: {
+            text: 'Age of patients',
+          },
+        },
+        yaxis: {
+          title: {
+            text: 'Number of cancer patients',
+          },
+        },
+        fill: {
+          opacity: 1,
+        },
+
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + ' patients';
+            },
+          },
+        },
+      },
+      series: [],
+    };
   },
   methods: {
-      getCancerCases: function () {
-        fetch(GeneralClasses.GetAPIBerlinCancerCases())
-        .then(response => response.json())
-        .then(data => {
+    getCancerCases: function () {
+      fetch(GeneralClasses.GetAPIBerlinCancerCases())
+        .then((response) => response.json())
+        .then((data) => {
           this.data = data.data;
-          this.initializeYears(data.data)
-          this.initializeAgeRanges(data.data)
-          })
-      },
+          this.initializeYears(data.data);
+          this.initializeAgeRanges(data.data);
+        });
+    },
 
     initializeYears: function (data) {
-      Object.entries(data).forEach(dataset => this.years.push(dataset[0]));
+      Object.entries(data).forEach((dataset) => this.years.push(dataset[0]));
     },
 
     initializeAgeRanges: function (data) {
-      Object.entries(data)[0][1].forEach(dataset => this.ages.push(dataset.age));
-      this.ages[this.ages.length -1] = "95 and older";
+      Object.entries(data)[0][1].forEach((dataset) => this.ages.push(dataset.age));
+      this.ages[this.ages.length - 1] = '95 and older';
       this.addDatesToCategories(this.ages);
       this.initializeSeries(data);
     },
 
     addDatesToCategories: function (ages) {
-      ages.forEach(age => {
+      ages.forEach((age) => {
         this.options.xaxis.categories.push(age);
-      })
+      });
       this.options.xaxis.categories.shift();
     },
 
     addCasesToSeries: function (data) {
-      let femaleCases = this.getDataPerGendre(data, "female");
-      let maleCases = this.getDataPerGendre(data, "male");
-      let totalCases = this.getDataPerGendre(data, "total");
-      this.series.push(
-        { name: 'Female', data: femaleCases },
-        { name: 'Male', data: maleCases },
-        { name: 'Total', data: totalCases },
-        );
+      let femaleCases = this.getDataPerGendre(data, 'female');
+      let maleCases = this.getDataPerGendre(data, 'male');
+      let totalCases = this.getDataPerGendre(data, 'total');
+      this.series.push({ name: 'Female', data: femaleCases }, { name: 'Male', data: maleCases }, { name: 'Total', data: totalCases });
     },
 
-    getDataPerGendre: function(data, gendre) {
+    getDataPerGendre: function (data, gendre) {
       let countSet = [];
-      data.forEach(d => countSet.push(d[gendre]))
+      data.forEach((d) => countSet.push(d[gendre]));
       return countSet;
     },
 
-    getDataOnChange: function(selectedYear) {
-      let filteredData = Object.entries(this.data).filter(dataset => dataset[0] === selectedYear);
+    getDataOnChange: function (selectedYear) {
+      let filteredData = Object.entries(this.data).filter((dataset) => dataset[0] === selectedYear);
       this.series = [];
       this.addCasesToSeries(filteredData[0][1]);
     },
 
     initializeSeries: function (data) {
-      this.addCasesToSeries(Object.entries(data)[0][1])
-    }
+      this.addCasesToSeries(Object.entries(data)[0][1]);
+    },
   },
 
   mounted() {
-      this.getCancerCases();
-  }
-}
+    this.getCancerCases();
+  },
+};
 </script>
 
 <style scoped>
-
-  h2 {
-    margin-top: 3rem;
-  }
-  h3 {
-    margin-bottom: 2rem;
-  }
-  /deep/ .v-application--wrap {
-    min-height: 0vh !important;
-    width: 30%;
-    margin-top: 0rem;
-  }
-  .container-fluid .col {
-    margin: 0;
-    padding: 0;
-  }
-  .row {
-    margin: 3rem 3rem 2rem 3rem;
-  }
-  #chart  {
-    box-shadow: 22px 22px 8px -3px #477FCD;
-  }
-  .box {
-    margin-left: 3rem;
-    height: 100%;
-  }
-  .app {
-    width: 10rem;
-    margin-left: 4rem;
-    padding: 15px;
-    min-height: 0vh;
-    margin-top: 0rem;
-    height: 20px;
-  }
-
+h2 {
+  margin-top: 3rem;
+}
+h3 {
+  margin-bottom: 2rem;
+}
+/deep/ .v-application--wrap {
+  min-height: 0vh !important;
+  width: 30%;
+  margin-top: 0rem;
+}
+.container-fluid .col {
+  margin: 0;
+  padding: 0;
+}
+.row {
+  margin: 3rem 3rem 2rem 3rem;
+}
+#chart {
+  box-shadow: 22px 22px 8px -3px #477fcd;
+}
+.box {
+  margin-left: 3rem;
+  height: 100%;
+}
+.app {
+  width: 10rem;
+  margin-left: 4rem;
+  padding: 15px;
+  min-height: 0vh;
+  margin-top: 0rem;
+  height: 20px;
+}
 </style>
--- src/views/BerlinCharts.vue
+++ src/views/BerlinCharts.vue
@@ -3,48 +3,41 @@
     <h2>Covid 19</h2>
     <h3>Covid 19 per age group</h3>
     <v-card class="card">
-    <v-app class="app">
-  <div class="back">
-    <div class="app">
-      <v-select
-            class="white border rounded-lg"
-            v-model="selectedDate"
-            :items="dates"
-            hide-details
-            item-text="name"
-            item-value="id"
-            label="Select a Day"
-            v-on:change="filterDataForDate(selectedDate)">
-      </v-select>
-    </div>
-  <div class="container-fluid mr-4 mb-4">
-    <div class="row">
-      <div class="col">
-        <v-data-table disable-sort :headers="this.headers" :items="this.data_table_data" :items-per-page="15" class="age"></v-data-table>
-      </div>
-      <div class="col">
-        <apexchart width="800" type="bar" :options="options" :series="series" class="chart white"></apexchart>
-          <Textbox :content="content" subtitle="Case numbers per age group" title="COVID-19" class="box" link="https://daten.berlin.de/datensaetze/covid-19-f%C3%A4lle-im-land-berlin-verteilung-nach-altersgruppen"/>
-     </div>
-    </div>
+
+        <div class="back">
+          <div class="app">
+            <v-select class="white border rounded-lg" v-model="selectedDate" :items="dates" hide-details item-text="name" item-value="id" label="Select a Day" v-on:change="filterDataForDate(selectedDate)"> </v-select>
+          </div>
+          <div class="container-fluid mr-4 mb-4">
+            <div class="row">
+              <div class="col">
+                <v-data-table disable-sort :headers="this.headers" :items="this.data_table_data" :items-per-page="15" class="age"></v-data-table>
+              </div>
+              <div class="col">
+                <apexchart width="800" type="bar" :options="options" :series="series" class="chart white"></apexchart>
+                <Textbox :content="content" subtitle="Case numbers per age group" title="COVID-19" class="box" link="https://daten.berlin.de/datensaetze/covid-19-f%C3%A4lle-im-land-berlin-verteilung-nach-altersgruppen" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+    </v-card>
   </div>
- </v-card>
- </div>
 </template>
 
 <script>
 import GeneralClasses from '../assets/GeneralClasses';
 import 'leaflet/dist/leaflet.css';
-import Textbox from "@/components/Textbox";
+import Textbox from '@/components/Textbox';
 
 export default {
   name: 'BerlinCharts',
-  components: {Textbox},
+  components: { Textbox },
   props: {},
 
   data() {
     return {
-      content: "Here you can easily determine the current distribution of COVID-19 cases in Berlin by age group. Age ranges are listed in the first and the corresponding number of COVID-19 cases in the second column. The third column shows you the difference in case numbers compared to the previous day, while the fourth column calculates the cumulative incidence per 100 000 inhabitants, which, among other things, is used to determine the current risk of infection as well as subsequent governmental policies. <br /><br />The column chart makes the relative distribution of cases per age range even more transparent, revealing a fairly steady rise of cases peaking at ages 30-39 and dropping again towards the oldest and most vulnerable age groups.</p>",
+      content: 'Here you can easily determine the current distribution of COVID-19 cases in Berlin by age group. Age ranges are listed in the first and the corresponding number of COVID-19 cases in the second column. The third column shows you the difference in case numbers compared to the previous day, while the fourth column calculates the cumulative incidence per 100 000 inhabitants, which, among other things, is used to determine the current risk of infection as well as subsequent governmental policies. <br /><br />The column chart makes the relative distribution of cases per age range even more transparent, revealing a fairly steady rise of cases peaking at ages 30-39 and dropping again towards the oldest and most vulnerable age groups.</p>',
       dates: [],
       datasets: [],
       data_table_data: [],
@@ -85,11 +78,11 @@ export default {
         this.datasets = Result.data.data;
         let initialData = Result.data.data[0].doc.data; //date: 22.11.2020
         this.initializeDatasetsForTableAndChart(initialData);
-      })
+      });
     },
 
     filterDataForDate(date) {
-      let filteredDataset = this.datasets.filter(dataset => dataset.doc.date === date)
+      let filteredDataset = this.datasets.filter((dataset) => dataset.doc.date === date);
       this.initializeDatasetsForTableAndChart(filteredDataset[0].doc.data);
     },
 
@@ -101,28 +94,28 @@ export default {
         if (dat['altersgruppe'] === ' Summe') {
           delete data[dat];
         } else {
-          value = parseInt(dat['fallzahl'])
+          value = parseInt(dat['fallzahl']);
           if (dat['altersgruppe'] === ' 0-4') {
-            temp = value
-            chart_data.pop()
+            temp = value;
+            chart_data.pop();
           }
           if (dat['altersgruppe'] === ' 5-9') {
-            value = value + temp
-            temp = 0
-            chart_data.pop()
+            value = value + temp;
+            temp = 0;
+            chart_data.pop();
           }
           if (dat['altersgruppe'] === ' 80-89') {
             temp = value;
           }
           if (dat['altersgruppe'] === ' 90+') {
-            value = value + temp
-            temp = 0
-            chart_data.pop()
+            value = value + temp;
+            temp = 0;
+            chart_data.pop();
           }
-          chart_data.push(value)
+          chart_data.push(value);
         }
         this.data_table_data = data;
-        this.updateChart(chart_data)
+        this.updateChart(chart_data);
       }
     },
     extractDates(data) {
@@ -150,8 +143,7 @@ export default {
 </script>
 
 <style scoped>
-
-.back{
+.back {
   background-color: white;
   background-image: url("data:image/svg+xml,%3Csvg width='504' height='288' viewBox='0 0 84 48' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M0 0h12v6H0V0zm28 8h12v6H28V8zm14-8h12v6H42V0zm14 0h12v6H56V0zm0 8h12v6H56V8zM42 8h12v6H42V8zm0 16h12v6H42v-6zm14-8h12v6H56v-6zm14 0h12v6H70v-6zm0-16h12v6H70V0zM28 32h12v6H28v-6zM14 16h12v6H14v-6zM0 24h12v6H0v-6zm0 8h12v6H0v-6zm14 0h12v6H14v-6zm14 8h12v6H28v-6zm-14 0h12v6H14v-6zm28 0h12v6H42v-6zm14-8h12v6H56v-6zm0-8h12v6H56v-6zm14 8h12v6H70v-6zm0 8h12v6H70v-6zM14 24h12v6H14v-6zm14-8h12v6H28v-6zM14 8h12v6H14V8zM0 8h12v6H0V8z' fill='%23477fcd' fill-opacity='0.07' fill-rule='evenodd'/%3E%3C/svg%3E");
 
@@ -171,7 +163,9 @@ export default {
 h2 {
   margin-top: 2rem;
 }
-h3 {margin-top: 2rem}
+h3 {
+  margin-top: 2rem;
+}
 .title {
   z-index: 1;
   position: absolute;
@@ -205,14 +199,14 @@ a {
   margin: 2rem 3rem 0 4rem;
 }
 .chart {
-  border-radius: .25rem;
-  border: 1px solid rgba(0,0,0,.125);
+  border-radius: 0.25rem;
+  border: 1px solid rgba(0, 0, 0, 0.125);
   margin: 0 2rem 0 1rem;
   box-shadow: 12px 12px 4px 0px #477fcdbe;
 }
 .age {
-  border-radius: .25rem;
-  border: 1px solid rgba(0,0,0,.125);
+  border-radius: 0.25rem;
+  border: 1px solid rgba(0, 0, 0, 0.125);
   margin: 0 1rem 0 2rem;
   box-shadow: 12px 12px 4px 0px #477fcdbe;
 }
@@ -223,7 +217,7 @@ a {
   margin-bottom: 1rem;
 }
 
-.v-input{
+.v-input {
   padding: 12px;
 }
 .card {
